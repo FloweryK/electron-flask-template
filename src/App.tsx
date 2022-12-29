80e9e4659d707ede857a8c9207908968cf19392a
FloweryK <flowerk94@gmail.com>
2022-12-30 00:34:28 +0900

    [feat] 서버 테스트 기능 추가, 서버 & 클라 CORS 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import axios from "axios";
+axios.defaults.withCredentials = true;
 
 function App() {
+  const [result, setResult] = useState("empty");
+
+  const onClick = () => {
+    axios
+      .get("http://127.0.0.1:5000")
+      .then((res) => {
+        setResult(res.data);
+      })
+      .catch((err) => {
+        setResult(err.data);
+      });
+  };
+
   return (
     <div className="App">
       <header className="App-header">
@@ -10,6 +25,7 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
+        <button onClick={onClick}>result: {result}</button>
         <a
           className="App-link"
           href="https://reactjs.org"
